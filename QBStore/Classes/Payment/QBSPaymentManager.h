fa4d0dc018e53cc95b8b1c7ee089b80b9d7bc102
Yu Xulu <tonyfish@qq.com>
2016-10-26 17:06:09 +0800

    Reverse commit b45c113

---
+++ QBStore/Classes/Payment/QBSPaymentManager.h
@@ -0,0 +1,23 @@
+//
+//  QBSPaymentManager.h
+//  Pods
+//
+//  Created by Sean Yue on 16/8/20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@class QBSOrder;
+
+typedef void (^QBSPaymentCompletionHandler)(QBSPaymentResult paymentResult, QBSOrder *order);
+
+@interface QBSPaymentManager : NSObject
+
+DeclareSingletonMethod(sharedManager)
+
+- (void)setup;
+- (void)handleOpenURL:(NSURL *)url;
+- (void)payForOrder:(QBSOrder *)order withCompletionHandler:(QBSPaymentCompletionHandler)completionHandler;
+
+@end
