8c2fdbcdaf47e3f6d90764caac36f6ad2e66a59d
ylz <yangliuzhong@ylz-2.local>
2017-01-05 15:34:47 +0800

    我的相册添加删除功能

---
+++ QBStore/Classes/MVCs/Mine/Model/JYNearPersonCell.h
@@ -0,0 +1,13 @@
+//
+//  JYNearPersonCell.h
+//  JYFriend
+//
+//  Created by ylz on 2016/12/22.
+//  Copyright © 2016年 Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JYNearPersonCell : UITableViewCell
+
+@end
---
+++ QBStore/Classes/MVCs/Mine/Model/JYNearPersonCell.m
@@ -0,0 +1,24 @@
+//
+//  JYNearPersonCell.m
+//  JYFriend
+//
+//  Created by ylz on 2016/12/22.
+//  Copyright © 2016年 Liang. All rights reserved.
+//
+
+#import "JYNearPersonCell.h"
+
+@implementation JYNearPersonCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
